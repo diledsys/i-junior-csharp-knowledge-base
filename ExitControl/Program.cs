5fd213fc8e5d2e0902fff13573b0e4158386966d
diledsys <diledsys.gmail.com>
2025-09-02 16:57:14 +0300

    feat(hw02): add new Project Exit Control

---
+++ ExitControl/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exit_Control
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exit Control");
+        }
+    }
+}
