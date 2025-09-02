2502e82eb35a022388e7cfc22a42ffc4bd2ddf69
diledsys <diledsys.gmail.com>
2025-09-02 20:24:38 +0300

    feat(hw01): add new project: Variables(hw_1)

---
+++ Variables(hw_1)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+using System.Text;
+
+namespace Exit_Control
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Значемые перемене
+            int age = 30;                                   // целое число
+            double weight = 72.5;                           // число с плавающй точкой
+            decimal price = 199.99m;                        // денежные расчеты
+            bool isActive = true;                           // буливо значение
+            char singleCharacterhter = 'S';                 // одиночный символ
+            float temperature = 36.6f;                      // число с плавающей точкой
+            long populationPlanetPeople = 8_000_000L;       // длинное целое
+            byte level = 3;                                 // уровень (0..255)
+
+            // Ссылочные переменные
+            String firstName = "Hromov";                    // строка
+            String lastName = "Dmitry";                     // строка
+
+            // Вывод в консоль значений
+            Console.WriteLine($"Significant changes \n--------------------\nage: {age} \nweight: {weight}\nprice: {price}\nisActive: {isActive}\nisActive: {isActive}" +
+                $"\nsingleCharacterhter: {singleCharacterhter}\ntemperature: {temperature}\npopulationPlanetPeople: {populationPlanetPeople}" +
+                $"\nlevel: {level}\n\nReference variablesn\n--------------------\nfirstName: {firstName}\nlastName: {lastName}");
+
+
+
+        }
+    }
+}
