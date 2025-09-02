198003ffe279cd1e98bc06e47c37e1c919214069
diledsys <diledsys.gmail.com>
2025-09-02 15:36:28 +0300

    feat(hw01): add MasteringCycles

---
+++ MasteringCycles/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mastering_Cycles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
